c19d4c5376c9b649a447a93fcbf71b92e66e76b1
G3 <purep@qq.com>
2023-07-14 15:08:35 +0800

    探究了openmp变量共享私有的关系 in omp_hello.c

--- codes_3/omp_hello.c
+++ codes_3/omp_hello.c
@@ -9,17 +9,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-   /* Fork threads, each print out a "Hello World" statement */
+	int j = 0;
+	#pragma omp parallel for
+	for (int i = 0; i < 10; ++i)
+	{
+		j += 2;
+		printf("    %d: ", omp_get_thread_num());
+//		int j = 2 * (i + 1);
+		printf("%d ", j);
+	}
+//  上方代码输出异常，输出的数都很大，如12 12 16 16 20等
+//  原因是变量j是在并行区域的外部定义的，它被所有线程共享。这可能导致多个线程同时读取和更新j的值，从而产生竞争条件。
+//  如多个线程都先执行了+2操作，最后再输出
 
-   #pragma omp parallel
-   {
-      int tid; // local variable
-
-      tid = omp_get_thread_num(); // get thread ID
-      printf("Hello World from thread = %d\n", tid);
-
-   }  /* end of parallel section */
-
+	printf("\n\n\n");
+	#pragma omp parallel for
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("    %d: ", omp_get_thread_num());
+		int j = 2 * (i + 1);
+		printf("%d ", j);
+	}
 }
